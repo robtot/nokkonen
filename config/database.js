77a9163f4f637c061f2e77eeb302b16300407d6a
Robert Rytovuori <robert.rytovuori@gmail.com>
2016-04-17 14:49:46 +0100

    configured database for modulus

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url': 'mongodb://localhost/nokkonen'
+  'url': 'mongodb://root:teT4etuqos@waffle.modulusmongo.net:27017/oro3Wawa?autoReconnect=true&connectTimeoutMS=60000'
 };
